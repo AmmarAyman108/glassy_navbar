61143dcb009e1c9194247d6cbc56851821f5e17e
Ammar Ayman <168241329+AmmarAyman108@users.noreply.github.com>
2025-06-26 15:25:49 +0300

    Initial commit

---
+++ lib/views/notifications_view.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:glassy_navbar/core/theme/colors_manager.dart';
+
+class NotificationsView extends StatelessWidget {
+  const NotificationsView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: ColorManager.primaryColor,
+      appBar: AppBar(
+        backgroundColor: ColorManager.primaryColor,
+        title: const Text('Notifications'),
+        foregroundColor: ColorManager.kWhiteColor,
+      ),
+      body: const Center(
+        child: Text(
+          'You have no new notifications.',
+          style: TextStyle(color: ColorManager.kWhiteColor, fontSize: 20),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/views/search_view.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:glassy_navbar/core/theme/colors_manager.dart';
+
+class SearchView extends StatelessWidget {
+  const SearchView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: ColorManager.primaryColor,
+      appBar: AppBar(
+        backgroundColor: ColorManager.primaryColor,
+        title: const Text('Search'),
+        foregroundColor: ColorManager.kWhiteColor,
+      ),
+      body: const Center(
+        child: Text(
+          'Find what you\'re looking for!',
+          style: TextStyle(color: ColorManager.kWhiteColor, fontSize: 20),
+        ),
+      ),
+    );
+  }
+}
